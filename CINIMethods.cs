59a8383a2788909336e926fa31e7e204c40b1356
Loritas <2223292817@qq.com>
2021-09-16 15:44:00 +0800

    Impement CINIMethods

--- CINIMethods.cs
+++ CINIMethods.cs
@@ -18,12 +18,74 @@ namespace FinalRelert2
             return itrSection.EntityDict[pKey];
         }
 
-        public static string GetString(string pSection, string pKey, Dictionary<string, CINISection> Dict, string pDefault = "")
+        public static string GetString(string pSection, string pKey, Dictionary<string, CINISection> Dict, string pDefault = null)
         {
             string res = TryGetString(pSection, pKey, Dict);
             if (res != null)
                 return res;
             return pDefault;
         }
+
+        public static int GetInteger(string pSection, string pKey, Dictionary<string, CINISection> Dict, int pDefault = 0)
+        {
+            string res = GetString(pSection, pKey, Dict);
+            if (res != null)
+                return Convert.ToInt32(res);
+            return pDefault;
+        }
+
+        public static float GetSingle(string pSection, string pKey, Dictionary<string, CINISection> Dict, float pDefault = 0)
+        {
+            string res = GetString(pSection, pKey, Dict);
+            if (res != null)
+                return Convert.ToSingle(res);
+            return pDefault;
+        }
+
+        public static double GetDouble(string pSection, string pKey, Dictionary<string, CINISection> Dict, double pDefault = 0)
+        {
+            string res = GetString(pSection, pKey, Dict);
+            if (res != null)
+                return Convert.ToDouble(res);
+            return pDefault;
+        }
+
+        public static bool GetBool(string pSection, string pKey, Dictionary<string, CINISection> Dict, bool nDefault = false)
+        {
+            string res = GetString(pSection, pKey, Dict);
+            switch (Convert.ToChar(res))
+            {
+                case '1':
+                case 'T':
+                case 'Y':
+                    return true;
+                case '0':
+                case 'F':
+                case 'N':
+                    return false;
+                default:
+                    return nDefault;
+            }
+        }
+
+        public static bool WriteString(string pSection, string pKey, string pValue,Dictionary<string, CINISection> Dict)
+        {
+            if (pSection == null || pKey == null)
+                return false;
+
+            if (Dict.ContainsKey(pSection))
+            {
+                if (Dict[pSection].EntityDict.ContainsKey(pKey))
+                    return false;
+                Dict[pSection].EntityDict[pKey] = pValue;
+                Dict[pSection].AddEntry(pKey, pValue);
+                return true;
+            }
+            CINISection section = new CINISection();
+            section.EntityDict[pKey] = pValue;
+            section.AddEntry(pKey, pValue);
+            return true;
+        }
+
     }
 }
